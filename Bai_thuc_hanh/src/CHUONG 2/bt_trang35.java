29d15e72518f2814b595d3db95d5d94d0e03e31b
Your_UserName <Your_Email>
2024-05-14 22:27:12 +0700

    chuong 2 trang 35

--- Bai_thuc_hanh/src/CHUONG 2/bt_trang35.java
+++ Bai_thuc_hanh/src/CHUONG 2/bt_trang35.java
@@ -9,9 +9,9 @@ public class bt_trang35 {
         System.out.println("nhap so thu hai :");
         sthai= sc.nextInt();
         if (stnhat > sthai) {
-            System.out.println("so nhon hon la :" +sthai);
+            System.out.println("so nho hon la :" +sthai);
         }else{
-            System.out.println("so nhon hon la :" +stnhat);
+            System.out.println("so lon hon la :" +stnhat);
         }
     }
 }
