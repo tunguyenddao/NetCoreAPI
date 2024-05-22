9b9b1fddb3721ddad00d918c6aba19e99226af5d
Your_UserName <Your_Email>
2024-05-22 09:54:20 +0700

    NDT733_Cau2

---
+++ Bai_kiem_tra/src/NDT733_Cau2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+    public class NDT733_Cau2 extends NDT733Person {
+        // int tuoi;
+        // float diemTB;
+        // String ten;
+        
+
+    public void nhaptt(){
+
+        System.out.println("nhap tuoi" ); 
+        
+        System.out.println("nhap diemTB"  );
+
+        System.out.println("nhap ten :");
+    }
+    public void intt(){
+        System.out.println("tuoi cua ban la :" + tuoi);
+        System.out.println("diem cua ban la :" + diemTB);
+        System.out.println("ten ban la :" + ten);
+    }
+}
