e13310021326ffbd71bbefc926bb4568a84dff88
Your_UserName <Your_Email>
2024-05-22 09:35:04 +0700

    NDT733_Cau1

---
+++ Bai_kiem_tra/src/Cau_1.java
@@ -0,0 +1,12 @@
+//tên class commit note bs họ và tên tắt + 3 số cuối msv  : NDT733   Linkedlist
+
+import java.util.Scanner;
+
+public class Cau_1 {
+    public class NDT733Person {
+        int tuoi;
+        String ten;
+        float diemTB;
+
+    }
+}
